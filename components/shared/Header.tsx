a5327844834d1a7d8febcf363ce405ecb548ca63
Vinayak <vinayakcpa@gmail.com>
2024-01-07 21:29:37 -0400

    added NavItems

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,10 +1,13 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "../ui/button";
+import NavItems from "./NavItems";
 
 const Header = () => {
   return (
     <header className="w-full border-b">
-      <div className="wrapper flex-center justify-between">
+      <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
           <Image
             src="/assets/images/logo.svg"
@@ -13,7 +16,23 @@ const Header = () => {
             alt="Evently logo"
           />
         </Link>
-        <div className="flex w-32 justify-end gap-3"></div>
+
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
       </div>
     </header>
   );
